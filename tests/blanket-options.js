55b356bbb3eb7058460d6b61e2bdfb0f38de4786
Matthew Dahl <mdahl@ciena.com>
2016-03-10 12:04:44 -0800

    Add code coverage

---
+++ tests/blanket-options.js
@@ -0,0 +1,25 @@
+/* globals blanket, module */
+
+var options = {
+  modulePrefix: 'ember-frost-app-bar',
+  filter: '//.*ember-frost-app-bar/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['lcov'],
+    autostart: true,
+    lcovOptions: {
+      outputFile: 'coverage/lcov.info',
+      renamer: function (fileName) {
+        return fileName.replace('ember-frost-app-bar', 'addon') + '.js'
+      }
+    }
+  }
+}
+
+if (typeof exports === 'undefined') {
+  blanket.options(options)
+} else {
+  module.exports = options
+}
